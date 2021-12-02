78331d4500eaed01c0231eaf16614624912b9ec3
987vinicius <msvinicius.ads@gmail.com>
2021-12-02 13:11:09 -0800

    remove column status:boolean, add status:integer em Laboratorios

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_02_210520) do
+ActiveRecord::Schema.define(version: 2021_12_02_210955) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,9 +33,9 @@ ActiveRecord::Schema.define(version: 2021_12_02_210520) do
     t.integer "cep"
     t.string "cidade"
     t.string "estado"
-    t.boolean "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "status"
   end
 
 end
