179329abee82b55f4b86755e80b2f16af432e505
987vinicius <msvinicius.ads@gmail.com>
2021-12-03 05:08:43 -0800

    heroku

--- config/application.rb
+++ config/application.rb
@@ -27,5 +27,7 @@ module LaboratorioAlliver
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+		config.assets.initialize_on_precompile = false
+		config.assets.js_compressor = Uglifier.new(:harmony => true)
 	end
 end
